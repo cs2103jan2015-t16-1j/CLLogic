d39c5d10a0902434b2a40380e0bff7edcee32d9e
Shao Fei <sfatchs@yahoo.com.sg>
2015-03-19 15:59:52 +0800

    no message

--- CommandParser.java
+++ CommandParser.java
@@ -1,4 +1,3 @@
-
 public class CommandParser {
 	
 	TaskAction _action;
@@ -7,7 +6,34 @@ public class CommandParser {
 	}
 	
 	public void processCmdString(String cmdString) {
+		String[] actionAndFields = OldCommandParser.splitActionAndFields(cmdString);
 		
+		String actionString = actionAndFields[0].trim();
+		ActionType  = getActionType(actionString);
+		
+		switch (ActionType) {
+		case SORT:
+			_action = new SortAction();
+			break;
+		case FIND:
+			_action = new FindAction();
+			break;
+		default:
+			return;
+		}
+		
+		String fieldsString = actionAndFields[1].trim();
+		LinkedList<Field> = _fieldParser.getFields(fieldsString);
 	}
-
+	
+	public static String[] splitActionAndFields(String command) {
+		String[] splittedInstruction = command.split(STRING_BLANK_SPACE, NUM_SPLIT_TWO);
+		if(splittedInstruction.length == 1) {
+			String action = splittedInstruction[INDEX_ACTION];
+			splittedInstruction = new String[2];
+			splittedInstruction[INDEX_ACTION] = action;
+			splittedInstruction[INDEX_FIELDS] = "";
+		}
+		return splittedInstruction;
+	}
 }
